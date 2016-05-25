a3ee964406b54fccc2c60921a19aac2feca29955
soarpenguin <soarpenguin@gmail.com>
2016-05-25 22:31:59 +0800

    update call command.

--- call.go
+++ call.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"strings"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
 
@@ -12,6 +15,42 @@ var callCommand = cli.Command{
 	Usage:     "run command in redis cluster.",
 	ArgsUsage: `host:port command arg arg .. arg`,
 	Action: func(context *cli.Context) error {
+		rt := NewRedisTrib()
+		if err := rt.CallClusterCmd(context); err != nil {
+			//logrus.Errorf("%p", err)
+			return err
+		}
 		return nil
 	},
+}
+
+func (self *RedisTrib) CallClusterCmd(context *cli.Context) error {
+	var addr string
+
+	if len(context.Args()) < 2 {
+		logrus.Fatalf("Must provide \"host:port command\" for call command!")
+	} else if addr = context.Args().Get(0); addr == "" {
+		logrus.Fatalf("Please check host:port for call command!")
+	}
+
+	if err := self.LoadClusterInfoFromNode(addr); err != nil {
+		return err
+	}
+
+	cmd := context.Args().Get(1)
+	args := strings.Join(context.Args()[2:], " ")
+	logrus.Printf(">>> Calling %s %s", cmd, args)
+	for _, node := range self.nodes {
+		if node == nil {
+			continue
+		}
+		//res, err := node.CallCmd(cmd, args)
+		//if err != nil {
+		//	logrus.Printf("%s: %v", node.String(), res)
+		//} else {
+		//	logrus.Printf("%s: %s", node.String(), err.Error())
+		//}
+		//return err
+	}
+	return nil
 }
