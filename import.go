20745359d711fe41e2da3811fdb296a0bfd23778
soarpenguin <soarpenguin@gmail.com>
2016-05-31 22:21:55 +0800

    update import command.

--- import.go
+++ import.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"errors"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
 
@@ -29,6 +32,41 @@ var importCommand = cli.Command{
 		},
 	},
 	Action: func(context *cli.Context) error {
+		if len(context.Args()) < 1 {
+			logrus.Fatalf("Must provide \"host:port\" for import command!")
+		}
+
+		rt := NewRedisTrib()
+		if err := rt.ImportClusterCmd(context); err != nil {
+			//logrus.Errorf("%p", err)
+			return err
+		}
 		return nil
 	},
+}
+
+func (self *RedisTrib) ImportClusterCmd(context *cli.Context) error {
+	var addr string
+	var source string
+
+	if source = context.String("from"); source == "" {
+		logrus.Fatalf("Option \"--from\" is required for import command!")
+	}
+
+	if addr = context.Args().Get(0); addr == "" {
+		return errors.New("Please check host:port for import command.")
+	}
+
+	logrus.Printf(">>> Importing data from %s to cluster %s", source, addr)
+
+	// Load nodes info before parsing options, otherwise we can't
+	// handle --weight.
+	if err := self.LoadClusterInfoFromNode(addr); err != nil {
+		return err
+	}
+
+	// Check cluster, only proceed if it looks sane.
+	self.CheckCluster(false)
+
+	return nil
 }
