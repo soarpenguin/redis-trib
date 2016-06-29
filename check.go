6df05e26b5ba040cea982b49897ecd90f7caa64e
soarpenguin <soarpenguin@gmail.com>
2016-06-29 22:34:11 +0800

    update check cmd.

--- check.go
+++ check.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-
 	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
@@ -12,26 +10,21 @@ var checkCommand = cli.Command{
 	Usage:     "check the redis cluster.",
 	ArgsUsage: `host:port`,
 	Action: func(context *cli.Context) error {
-		var addr string
-
-		if len(context.Args()) < 1 {
-			logrus.Fatalf("Must provide host:port for check command!")
-		} else if addr = context.Args().Get(0); addr == "" {
-			logrus.Fatalf("Please check host:port for check command!")
-		}
-
 		rt := NewRedisTrib()
-		if err := rt.CheckClusterCmd(addr); err != nil {
-			//logrus.Errorf("%p", err)
+		if err := rt.CheckClusterCmd(context); err != nil {
 			return err
 		}
 		return nil
 	},
 }
 
-func (self *RedisTrib) CheckClusterCmd(addr string) error {
-	if addr == "" {
-		return errors.New("Please check host:port for check command.")
+func (self *RedisTrib) CheckClusterCmd(context *cli.Context) error {
+	var addr string
+
+	if len(context.Args()) < 1 {
+		logrus.Fatalf("Must provide host:port for check command!")
+	} else if addr = context.Args().Get(0); addr == "" {
+		logrus.Fatalf("Please check host:port for check command!")
 	}
 
 	if err := self.LoadClusterInfoFromNode(addr); err != nil {
